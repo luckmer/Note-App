e4c06f7dffc635548ccaeb39ca7c56600f2575e3
luckmer <woszczo04@gmail.com>
2021-05-01 14:20:48 +0200

    design update

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,12 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
 
 export const StoreContext = createContext();
 
 const Store = ({ children }) => {
-  const [data, setData] = useState([]);
-  const store = {
-    details: [data, setData],
-  };
+  const store = {};
 
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
