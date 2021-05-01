775a29c8495fead4a899f3f73aebd5ecabe1e4de
luckmer <woszczo04@gmail.com>
2021-05-01 14:11:00 +0200

    title

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-## 
-
-Note
+##
+Note-app
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import GlobalStyle from "./style/global/Global";
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById("root")
 );
