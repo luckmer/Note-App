e533dee62de4d3096bc259c017d9b4dad9d029af
luckmer <woszczo04@gmail.com>
2021-05-01 14:19:17 +0200

    sidebar update

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,18 +6,45 @@ const Section = styled.section`
   background-color: #f2f1f7;
   min-width: 300px;
   border-right: 1px solid #d9d8dd;
+  display: flex;
+  flex-direction: column;
 `;
+
+const Header = styled.header`
+  display: flex;
+  margin: 5px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding: 15.4px;
+`;
+
+const Span = styled.span`
+  line-height: 1;
+  font-size: 20px;
+  color: #333;
+  font-family: monospace;
+  font-weight: bold;
+  margin: 0 0.8rem;
+  padding: 0.2rem 0.8rem;
+  cursor: pointer;
+`;
+
+const Data = styled.section`
+  width: 100%;
+  height: 100%;
+`;
+
+const Div = styled.div``;
 
 function SideBar() {
   return (
     <Section>
-      <header>
-        <span>Search</span>
-        <div></div>
-      </header>
-      <section>
-        <div></div>
-      </section>
+      <Header>
+        <Span>Search</Span>
+      </Header>
+      <Data>
+        <Div></Div>
+      </Data>
     </Section>
   );
 }
