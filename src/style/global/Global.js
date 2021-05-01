dd14897bf666e5e0d49e440ae399b6d16563a9e3
Your Name <woszczo04@gmail.comtandeta23>
2021-05-01 13:17:43 +0200

    style

---
+++ src/style/global/Global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+*{
+  padding:0;
+  box-sizing:border-box;
+}
+
+body{
+  margin:0;
+}
+
+`;
+
+export default Global;
