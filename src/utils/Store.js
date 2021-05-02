019ad8596fc4b08156f8fb80acdf2005580505fe
luckmer <woszczo04@gmail.com>
2021-05-02 17:18:55 +0200

    store data update

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,9 +1,57 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
+import { useEditor } from "../hooks/Draft";
+import { useDragging } from "../hooks/UseDragging";
+import { getPosition } from "../hooks/GetPosition";
 
 export const StoreContext = createContext();
 
 const Store = ({ children }) => {
-  const store = {};
+  const [openMenu, setOpenMenu] = useState(false);
+  const [ref, x, y, width, height] = useDragging(openMenu);
+  const [X, Y, ControlXRadius, ControlYRadius] = getPosition(
+    y,
+    x,
+    height,
+    width
+  );
+  const [
+    notes,
+    currentNoteId,
+    editorState,
+    setEditorState,
+    addNote,
+    openNote,
+    deleteNote,
+    toggleBlockType,
+    toggleInlineStyle,
+  ] = useEditor();
+
+  const store = {
+    DRAFT: {
+      notes,
+      currentNoteId,
+      editorState,
+      setEditorState,
+      addNote,
+      openNote,
+      deleteNote,
+      toggleBlockType,
+      toggleInlineStyle,
+    },
+    MENU: {
+      setOpenMenu,
+      openMenu,
+      ref,
+      x,
+      y,
+      width,
+      height,
+      X,
+      Y,
+      ControlXRadius,
+      ControlYRadius,
+    },
+  };
 
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
