b5862d15e397adb0319e9d3363f2aa5904dfe8ba
luckmer <woszczo04@gmail.com>
2021-05-02 17:49:54 +0200

    global style change

--- src/style/global/Global.js
+++ src/style/global/Global.js
@@ -10,6 +10,24 @@ body{
   margin:0;
 }
 
+.styleButton {
+  color: #999;
+  cursor: pointer;
+  margin-right: 16px;
+  padding: 2px 0;
+  display: inline-block;
+}
+
+.activeButton {
+  color: #5890ff;
+}
+
+.DraftEditor-root{
+  height:20vh;
+  margin: 20px;
+  padding: 5px;
+}
+
 `;
 
 export default Global;
