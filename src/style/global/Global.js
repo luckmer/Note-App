ee6d189aa5f0bf2f09bd23a28418c4a80cef2672
luckmer <woszczo04@gmail.com>
2021-05-02 18:19:44 +0200

    global update

--- src/style/global/Global.js
+++ src/style/global/Global.js
@@ -11,22 +11,51 @@ body{
 }
 
 .styleButton {
+  font-weight: 700;
   color: #999;
   cursor: pointer;
   margin-right: 16px;
-  padding: 2px 0;
+  padding: 10px 0;
+  line-height:1em;
+  font-family: 'Monaco', monospace;
   display: inline-block;
 }
 
 .activeButton {
-  color: #5890ff;
+  color:#0380a1;
 }
 
 .DraftEditor-root{
   height:20vh;
   margin: 20px;
   padding: 5px;
+  word-break: break-all; 
+  font-family: 'Monaco',monospace;
 }
+
+.public-DraftStyleDefault-ul , .public-DraftStyleDefault-ol , blockquote{
+  background-color:#fc929e;
+  color:#fff;
+  padding: 20px 20px 20px 25px;
+  border-radius:5px;
+  line-height:2em;
+  font-size:18px;
+  font-family: 'Monaco', monospace;
+}
+
+.public-DraftStyleDefault-ol{
+  padding-left:32px;
+  background-color:#c5a5c5;
+  color:#fff;
+}
+
+
+blockquote{
+  padding-left:32px;
+  background-color:#79b6f2;
+  color:#fff;
+}
+
 
 `;
 
