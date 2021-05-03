63613ddbd9ccbd52b495e9e2ab5bf2c9f2a44301
luckmer <woszczo04@gmail.com>
2021-05-03 13:59:43 +0200

    main sidebar panel

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,67 +1,41 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import * as _ from "../../style/components/SideBar.Style";
 import { StoreContext } from "../../utils/Store";
-
-const Return = (obj, current, data) => {
-  return {
-    ...data,
-    name: obj && obj.name,
-    category: obj && obj.category,
-    id: current && current,
-  };
-};
+import MapByCategory from "./MapByCategory";
 
 function SideBar() {
-  const { DRAFT } = useContext(StoreContext);
-  const { notes, OpenById, setNotes } = DRAFT;
-  // const [detect, setDetect] = useState(false);
-  // const [categories, setCategories] = useState([]);
-  // const [dragData, setDragData] = useState({ current: "", target: "" });
-  // const [Update, setUpdate] = useState([]);
-  // const [DroppedData, setDroppedData] = useState([]);
-
-  // useEffect(() => {
-  //   const category = notes.map((item) => item.alter);
-  //   setCategories(category);
-  // }, [notes]);
-
-  // const categoriesPanel = [...new Set(categories)];
-
-  // const Style = (e) => e.target.classList.remove("Click");
-  // const onDragOver = () => {};
+  const { DRAFT, DROP, CATEGORY } = useContext(StoreContext);
+  const { notes } = DRAFT;
+  const {
+    onDragOver,
+    Style,
+    onDrop,
+    setDetect,
+    UpdateDragWay,
+    DragControl,
+  } = DROP;
+  const { categoriesPanel } = CATEGORY;
 
   return (
     <_.Section>
       <_.Header>
-        <_.Span>Search</_.Span>
+        <_.Span>Notes</_.Span>
       </_.Header>
       <_.Data>
         <_.Div>
-          {/* {categoriesPanel.map((data, i) => {
-            const FilterByCategory = notes.filter(
-              (item) => item.category.trim() === data.trim()
-            );
-            return (
-              <div key={data}>
-                <div>
-                  <header>{data}</header>
-                </div>
-                <div>
-                  {FilterByCategory.map((name, i) => (
-                    <div key={i} id={name.id} draggable>
-                      <span>
-                        <div>
-                          <div id={name.id}>
-                            <p>{name.name}</p>
-                          </div>
-                        </div>
-                      </span>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            );
-          })} */}
+          {categoriesPanel.map((data, i) => (
+            <MapByCategory
+              key={i}
+              data={data}
+              onDragOver={onDragOver}
+              Style={Style}
+              notes={notes}
+              onDrop={onDrop}
+              setDetect={setDetect}
+              UpdateDragWay={UpdateDragWay}
+              DragControl={DragControl}
+            />
+          ))}
         </_.Div>
       </_.Data>
     </_.Section>
