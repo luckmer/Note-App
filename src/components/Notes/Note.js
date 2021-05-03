8dc04268c9144d07dd293c0ff8c3ade295752107
luckmer <woszczo04@gmail.com>
2021-05-03 13:59:05 +0200

    main notes panel

--- src/components/Notes/Note.js
+++ src/components/Notes/Note.js
@@ -5,15 +5,26 @@ import { StoreContext } from "../../utils/Store";
 import { Editor } from "draft-js";
 
 function Note() {
-  const { DRAFT } = useContext(StoreContext);
+  const { DRAFT, DROP } = useContext(StoreContext);
+  const { dragData } = DROP;
   const {
     editorState,
     setEditorState,
     toggleBlockType,
     toggleInlineStyle,
+    notes,
+    openNote,
   } = DRAFT;
 
   const handleChange = (editorState) => setEditorState(editorState);
+
+  const onDragOver = (e) => e.preventDefault();
+
+  const Drop = () => {
+    const Id = dragData.current;
+    const Find = notes.find((el) => el.id === Id);
+    openNote(Find.id);
+  };
 
   return (
     <_.Section>
@@ -30,8 +41,8 @@ function Note() {
         </_.NavDiv>
       </_.Nav>
       <_.DivNote>
-        <_.Div>
-          <_.NoteDiv id="here">
+        <_.Div onDragOver={(e) => onDragOver(e)} onDrop={() => Drop()}>
+          <_.NoteDiv>
             <Editor editorState={editorState} onChange={handleChange} />
           </_.NoteDiv>
         </_.Div>
