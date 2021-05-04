5e4dfb05e5f6d69ba03c250e69afed363625a0dc
luckmer <woszczo04@gmail.com>
2021-05-04 12:19:38 +0200

    mobile menu

---
+++ src/components/menu/Menu.js
@@ -0,0 +1,13 @@
+import MenuOptions from "./MenuOptions";
+import WrapperControl from "./WrapperControl";
+
+function Menu() {
+  return (
+    <>
+      <MenuOptions />
+      <WrapperControl />
+    </>
+  );
+}
+
+export default Menu;
