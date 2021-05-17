459cc9717a18e927d4a8e71195933cb0fd8d15c6
luckmer <woszczo04@gmail.com>
2021-05-17 11:11:55 +0200

    SideBSidebarDrag optimization

--- src/hooks/SidebarDrag.js
+++ src/hooks/SidebarDrag.js
@@ -1,13 +1,4 @@
-import { useState, useEffect } from "react";
-
-const Return = (obj, current, data) => {
-  return {
-    ...data,
-    name: obj && obj.name,
-    category: obj && obj.category,
-    id: current && current,
-  };
-};
+import { useState, useEffect, useCallback } from "react";
 
 const Style = (e) => e.target.classList.remove("Click");
 
@@ -16,74 +7,105 @@ const DragEndDrop = (notes, setNotes) => {
   const [detect, setDetect] = useState(false);
   const [Update, setUpdate] = useState([]);
 
-  const onDrop = (e, cat) => {
-    const id = e.dataTransfer.getData("id");
-    const tasks = UpdateArray(id, cat);
-    setNotes(tasks);
-    Style(e);
-  };
+  const Return = useCallback((obj, current, data) => {
+    return {
+      ...data,
+      name: obj && obj.name,
+      category: obj && obj.category,
+      id: current && current,
+    };
+  }, []);
 
-  const UpdateArray = (id, cat) => {
-    return notes.filter((data) => {
-      if (data.name === id) data.category = cat;
-      return data;
-    });
-  };
-
-  const onDragStart = (e, id) => {
-    e.dataTransfer.setData("id", id);
-
-    setDragData({
-      ...dragData,
-      current: e.target.id,
-    });
-  };
-
-  const onDragOver = (e) => {
-    e.preventDefault();
-
-    if (e.target.classList.value === "sc-bqiQRQ boJAzj") {
-      e.target.classList.add("Click");
-    }
-
-    setDragData({
-      ...dragData,
-      target: e.target.id,
-    });
-  };
-
-  const DragControl = (e, name) => {
-    setDetect(!detect);
-    onDragStart(e, name.name);
-  };
-
-  function DataBlock(notes, e) {
-    const current = dragData.current;
-    const target = e.target.id;
-
-    const One = notes.find((el) => el.id === current);
-    const two = notes.find((el) => el.id === target);
-    const OneBlocker = One && One.category;
-    const TwoBlocker = two && two.category;
-
-    return [OneBlocker, TwoBlocker, One, two, target, current];
-  }
-
-  const UpdateDragWay = (e) => {
-    const [OneBlocker, TwoBlocker, One, two, target, current] = DataBlock(
-      notes,
-      e
-    );
-    if (OneBlocker === TwoBlocker) {
-      const update = notes.map((data) => {
-        if (target === data.id) return Return(One, current, data);
-        if (current === data.id) return Return(two, target, data);
+  const UpdateArray = useCallback(
+    (id, cat) => {
+      return notes.filter((data) => {
+        if (data.name === id) data.category = cat;
         return data;
       });
+    },
+    [notes]
+  );
 
-      setUpdate(update);
-    }
-  };
+  const onDrop = useCallback(
+    (e, cat) => {
+      const id = e.dataTransfer.getData("id");
+      const tasks = UpdateArray(id, cat);
+      setNotes(tasks);
+      Style(e);
+    },
+    [setNotes, UpdateArray]
+  );
+
+  const onDragStart = useCallback(
+    (e, id) => {
+      e.dataTransfer.setData("id", id);
+
+      setDragData({
+        ...dragData,
+        current: e.target.id,
+      });
+    },
+    [dragData]
+  );
+
+  const onDragOver = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (e.target.classList.value === "sc-bqiQRQ boJAzj") {
+        e.target.classList.add("Click");
+      }
+
+      setDragData({
+        ...dragData,
+        target: e.target.id,
+      });
+    },
+    [dragData]
+  );
+
+  const DragControl = useCallback(
+    (e, name) => {
+      setDetect(!detect);
+      onDragStart(e, name.name);
+    },
+    [detect, onDragStart]
+  );
+
+  const DataBlock = useCallback(
+    (notes, e) => {
+      const current = dragData.current;
+      const target = e.target.id;
+
+      const One = notes.find((el) => el.id === current);
+      const two = notes.find((el) => el.id === target);
+      const OneBlocker = One && One.category;
+      const TwoBlocker = two && two.category;
+
+      return [OneBlocker, TwoBlocker, One, two, target, current];
+    },
+    [dragData]
+  );
+
+  const UpdateDragWay = useCallback(
+    (e) => {
+      const [OneBlocker, TwoBlocker, One, two, target, current] = DataBlock(
+        notes,
+        e
+      );
+
+      if (OneBlocker === TwoBlocker) {
+        const update = notes.map((data) => {
+          if (target === data.id) return Return(One, current, data);
+          if (current === data.id) return Return(two, target, data);
+          return data;
+        });
+
+        setUpdate(update);
+      }
+    },
+    [notes, DataBlock, Return]
+  );
 
   useEffect(() => {
     Update.length && setNotes(Update);
